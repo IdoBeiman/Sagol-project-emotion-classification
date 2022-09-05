9fde47b1fc3bdf29787932ec2e5428416d7abd13
IdoBeiman <83544458+IdoBeiman@users.noreply.github.com>
2022-09-05 10:40:20 +0300

    .

--- Scripts/Annotation Analysis and ML/direct_model_compare.py
+++ Scripts/Annotation Analysis and ML/direct_model_compare.py
@@ -28,15 +28,15 @@ def run():
                 for model_type in model_types:
                     if model_type == "dense":
                         # grid_params=get_optimal_model_params(pre_processed_df,s,model_type) # not running grid search since those were found as best
-                        SNN = MLmodel(n1=128,n2=128,d_o=0.6,ac_func="sigmoid", weight_constraint=1.0,initializer='uniform',model_type='dense', name='SNN') 
+                        SNN = MLmodel(n1=128,n2=64,d_o=0.6,ac_func="sigmoid",initializer='uniform',model_type='dense', name='SNN') 
                         # SNN = MLmodel(n1=128,n2=64,d_o=grid_params["model__dropout_rate"],ac_func=grid_params["model__activation"], weight_constraint=grid_params["model__weight_constraint"],model_type='dense', name='SNN')
                     elif model_type == "uniLSTM":
                         # grid_params=get_optimal_model_params(pre_processed_df,s,model_type) 
                         # uniLSTM = MLmodel(n1=32,n2=20,d_o=grid_params.dropout_rate,ac_func=grid_params.activation,model_type='uniLSTM',name='uLSTM')
-                        uniLSTM = MLmodel(n1=64,n2=32,d_o=0.6,ac_func="sigmoid",model_type='uniLSTM',name='uLSTM')
+                        uniLSTM = MLmodel(n1=32,n2=20,d_o=0.6,ac_func="sigmoid",model_type='uniLSTM',name='uLSTM')
                     elif model_type == "BiLSTM":
                         # BiLSTM = MLmodel(n1=16,n2=16,d_o=grid_params.dropout_rate,ac_func=grid_params.activation,model_type='BiLSTM',name='BiLSTM')
-                        BiLSTM = MLmodel(n1=16,n2=16,d_o=0.4,ac_func="sigmoid",model_type='BiLSTM',name='BiLSTM')
+                        BiLSTM = MLmodel(n1=16,n2=16,n3=8,d_o=0.4,ac_func="sigmoid",model_type='BiLSTM',name='BiLSTM')
 
                 Linear = MLmodel(name="Linear")
                 Baseline = MLmodel(name='BL')
@@ -46,6 +46,7 @@ def run():
                 iterations = get_num_splits(podcast_df, True)
                 current_iteration=1
                 row = {'Story':extract_details_from_file_name(podcast)}
+                # row = {'Story':"merged"}
                 accumulatedData ={}
                 for train_indexes, test_indexes in split_data_using_cross_validation(podcast_df, s,True): # true for random split
                     print (f"iteration {current_iteration} out of {iterations}.")
--- Scripts/Annotation Analysis and ML/model_class.py
+++ Scripts/Annotation Analysis and ML/model_class.py
@@ -5,6 +5,7 @@ from help_methods import *
 from tensorflow.keras.constraints import MaxNorm
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Bidirectional
+from tensorflow import keras
 import numpy as np
 from sklearn import linear_model, metrics
 from math import sqrt
@@ -14,9 +15,10 @@ from sklearn.metrics import mean_squared_error, r2_score
 class MLmodel:
 
     models = []
-    def __init__(self, n1=0,n2=0,d_o=0.1,ac_func="tanh",model_type="",n_epochs=8,name="",initializer="",weight_constraint=""):
+    def __init__(self, n1=0,n2=0,n3=0,d_o=0.1,ac_func="tanh",model_type="",n_epochs=8,name="",initializer="",weight_constraint=""):
         self.n1 = n1
         self.n2 = n2
+        self.n3=n3
         self.d_o = d_o # dropout
         self.ac_func = ac_func
         self.initializer=initializer
@@ -34,7 +36,7 @@ class MLmodel:
     def init_model(self,X):
         new_model = Sequential()
         if (self.model_type=="dense"):
-            new_model.add(Dense(self.n1,kernel_initializer=self.initializer, activation=self.ac_func,kernel_constraint=MaxNorm(self.weight_constraint)))
+            new_model.add(Dense(self.n1,kernel_initializer=self.initializer, activation=self.ac_func))
             new_model.add(Dropout(self.d_o))
             new_model.add(Dense(self.n2,kernel_initializer=self.initializer, activation=self.ac_func))
         if (self.model_type=="uniLSTM"):
@@ -45,8 +47,11 @@ class MLmodel:
             new_model.add(Bidirectional(LSTM(self.n1, stateful=True, return_sequences=True, activation=self.ac_func),batch_input_shape=(1, X.shape[1], X.shape[2])))
             new_model.add(Dropout(self.d_o))
             new_model.add(Bidirectional(LSTM(self.n2, stateful=True, return_sequences=True, activation=self.ac_func)))
+            new_model.add(Dropout(self.d_o))
+            new_model.add(Bidirectional(LSTM(self.n3, stateful=True, return_sequences=True, activation=self.ac_func)))
         new_model.add(Dense(1))
-        new_model.compile(loss='mean_squared_error', optimizer='adam')
+        opt = keras.optimizers.Adam(learning_rate=0.003)
+        new_model.compile(loss='mean_squared_error', optimizer=opt)
         return new_model
 
     def fit_NN(self, train_df,show_progress=False, n_epochs=8):
