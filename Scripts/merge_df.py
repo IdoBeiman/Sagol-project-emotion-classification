b55a3bfcc5963a53ed13eb81434ac79b25cea0c8
yulidb <yuli.dekel@gmail.com>
2022-09-09 12:49:17 +0300

    pre train

--- Scripts/merge_df.py
+++ Scripts/merge_df.py
@@ -1,17 +1,41 @@
 import pandas as pd
 
-df005 = pd.read_csv('C:\\Users\\Yuli\\Documents\\Uni\\tokenized-data\\by-model\\gpt2\\layer11\\mean\\data_activations_layer_11_sub-005_operation_mean_origin_full_with_context_model_gpt.csv')
-df006 = pd.read_csv('C:\\Users\\Yuli\\Documents\\Uni\\tokenized-data\\by-model\\gpt2\\layer11\\mean\\data_activations_layer_11_sub-006_operation_mean_origin_full_with_context_model_gpt.csv')
-df007 = pd.read_csv('C:\\Users\\Yuli\\Documents\\Uni\\tokenized-data\\by-model\\gpt2\\layer11\\mean\\data_activations_layer_11_sub-007_operation_mean_origin_full_with_context_model_gpt.csv')
+subjects = ['sub-005', 'sub-006', 'sub-007']
+language_models = ['bert', 'gpt', 't5']
+layers = [i for i in range(12)]
 
-# df005['Subject'] = 'sub-005'
-# df006['Subject'] = 'sub-006'
-# df007['Subject'] = 'sub-007'
-#
-# df005['episodeName'].update(['SUB005-' + val for val in df005['episodeName'].values])
-# df006['episodeName'].update(['SUB006-' + val for val in df006['episodeName'].values])
-# df007['episodeName'].update(['SUB007-' + val for val in df007['episodeName'].values])
+path_to_origin_folder = 'C:/Users/Yuli/Documents/Uni/tokenized-data/by-model'
 
-df = pd.concat([df005, df006, df007])
-df.to_csv('C:\\Users\\Yuli\\Documents\\Uni\\tokenized-data\\by-model\\gpt2\\layer11\\mean\\data_activations_layer_11_all_sub_operation_mean_origin_full_with_context_model_gpt.csv', index=False)
+file_format = '{path}/{model}/layer{l}/{op}/data_activations_layer_{l}_{sub}_operation_{op}_origin_full_with_context_model_{model}.csv'
+merged_file_format = '{path}/data_activations_layer_{l}_{sub}_operation_{op}_origin_full_with_context_model_{model}.csv'
+
+path_to_dest_folder = 'C:/Users/Yuli/Documents/Uni/tokenized-data/merged-all-sub'
+
+operation = 'mean'
+
+
+for model in language_models:
+    for layer in layers:
+        if model == 't5' and layer > 6:
+            continue
+        full_df = pd.DataFrame()
+        for sub in subjects:
+            sub_df = pd.read_csv(file_format.format(path=path_to_origin_folder, model=model, l=layer, op=operation, sub=sub))
+            sub_df['episodeName'].update([f'{sub}-{val}' for val in sub_df['episodeName'].values])
+            full_df = pd.concat([full_df, sub_df])
+        full_df.to_csv(merged_file_format.format(path=path_to_dest_folder, model=model, l=layer, op=operation, sub='all_sub'), index=False)
+print('done')
+
+
+# for layer in layers:
+#     full_df = pd.DataFrame()
+#     for sub in subjects:
+#         sub_df = pd.read_csv(
+#             '{path}/data_activations_layer_{l}_{sub}_operation_{op}_origin_full_with_context_model_{model}.csv'.format(
+#                 path='C:/Users/Yuli/Documents/Uni/tokenized-data/gpt-pre-train', model='gpt2-pretrained', l=layer, op=operation, sub=sub))
+#         sub_df['episodeName'].update([f'{sub}-{val}' for val in sub_df['episodeName'].values])
+#         full_df = pd.concat([full_df, sub_df])
+#     full_df.to_csv(
+#         merged_file_format.format(path=path_to_dest_folder, model='gpt2-pretrained', l=layer, op=operation, sub='all_sub'),
+#         index=False)
 
